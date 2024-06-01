405c1201fb8b1f0b5b073cdaa0a2b25060a2e92d
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-01 14:18:00 +0530

    pricing

--- components/Pricing/Pricing.js
+++ components/Pricing/Pricing.js
@@ -4,7 +4,7 @@ import styles from "./styles/Pricing.module.css"
 export default function Pricing() {
     return (
         <section className={styles.container}>
-            <div>
+            <div className={styles.element}>
                 <h3 className={styles.header}>Pricing Structure</h3>
                 <div className={styles.priceContainer}>
                     <PriceBox unitName='2 BHK' unit='2 BHK' area='1270 Sq.Ft.' price='1.27 Cr* Onwards'/>
