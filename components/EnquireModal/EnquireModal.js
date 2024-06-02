94d4437d5095aeb676b06b43d3f84684e59f3893
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-02 13:40:46 +0530

    enquire modal

--- components/EnquireModal/EnquireModal.js
+++ components/EnquireModal/EnquireModal.js
@@ -3,24 +3,10 @@ import Modal from 'react-modal'
 import styles from "./styles/EnquireModal.module.css"
 
 const EnquireModal = ({isOpen, setIsOpen}) => {
-   const customStyles = {
-      overlay: {
-         backgroundColor: 'rgba(0, 0, 0, 0.6)'
-      },
-      content: {
-         top: '50%',
-         left: '50%',
-         right: 'auto',
-         bottom: 'auto',
-         marginRight: '-50%',
-         transform: 'translate(-50%, -50%)',
-         width: '600px'
-      }
-   }
    return (
       <div>
-         <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
-            <div className={styles.header}>Download Brochure Now</div>
+         <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} className={styles.modal} overlayClassName={styles.overlay}>
+            <div className={styles.header}>Download Brochure</div>
             <form className={styles.form} action="https://api.web3forms.com/submit" method="POST">
                 <input type="hidden" name="access_key" value="e0445da9-5959-4e89-88cf-0b1a0d2c5c54" />
                 <div className={styles.formGroup}>
