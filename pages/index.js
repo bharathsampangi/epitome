5cf72264dede762b7216f6b4d8e035944fd49755
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-07 14:21:05 +0530

    sumadhura

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,14 @@ export default function Home() {
         <title>Sumadhura Epitome Rachenahalli, Price/Reviews by Sumadhura Group</title>
         <meta charset="utf-8"/>
         <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-        <meta name="description" content="Sumadhura Folium is a luxury high-rise apartment project in Whitefield, Bangalore. Get all details about Price/Locations/Status and apartments from here." />
+        <meta name="description" content="Sumadhura Epitome is a Brand New Residential Project at Bangalore by Sumadhura Group. It Offers 2 & 3 BHK Apartments. For More Details Contact Us Now." />
         <meta name="keywords" content="Sumadhura Folium,Sumadhura folium Whitefield,Sumadhura folium price, Sumadhura folium location,Sumadhura Folium Reviews,Sumadhura Group,Sumadhura folium Borewell Road,sumadhura folium Brochures "/>
         <link rel="icon" href="/favicon.ico" />
         <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://sumadhuraepitome.in.net/"></link>
         <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
         <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
-        <link rel="canonical" href="https://foliumbysumadhura.co.in/"></link>
+        <link rel="shortlink" href="https://sumadhuraepitome.in.net/"></link>
       </Head>
 
       <Main />
