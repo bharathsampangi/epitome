d0cdd02745acaab11d18c88c9cc16a5c092eb3a8
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-08 18:53:54 +0530

    icons

--- components/Amenities/Amenities.js
+++ components/Amenities/Amenities.js
@@ -1,4 +1,4 @@
-import styles from "./styles/Amenities.module.css"
+import styles from "./styles/Amenities.module.css";
 
 export default function Amenities() {
     return (
@@ -7,24 +7,98 @@ export default function Amenities() {
                 <h3 className={styles.header}>Top-tier Amenities</h3>
                 <div className={styles.amenitiesContainer}>
                     <ul className={styles.list}>
-                        <li>Club House</li>
-                        <li>Kids Play Area</li>
-                        <li>Swimming Pool</li>
-                        <li>Gym</li>
-                        <li>Open Space</li>
-                        <li>Power Backup</li>
-                        <li>Car Parking</li>
-                        <li>3 Tier Security</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/club.svg"
+                            />
+                            Club House
+                        </li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/children.svg"
+                            />Kids Play Area</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/pool.svg"
+                            />
+                            Swimming Pool
+                        </li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/gym.svg"
+                            />
+                            Gym
+                        </li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/park.svg"
+                            />
+                            Open Space
+                        </li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/power.svg"
+                            />Power Backup</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/parking.svg"
+                            />Car Parking</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/security.svg"
+                            />
+                            3 Tier Security
+                        </li>
                     </ul>
                     <ul className={styles.list}>
-                        <li>Vastu Compliant</li>
-                        <li>Multipurpose Hall</li>
-                        <li>Video Security</li>
-                        <li>Piped Gas</li>
-                        <li>Lift</li>
-                        <li>24X7 Security</li>
-                        <li>Jogging Track</li>
-                        <li>Maintenance Staff</li>
+                    <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/nature.svg"
+                            />Vastu Compliant</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/hall.svg"
+                            />Multipurpose Hall</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/cctv.svg"
+                            />Video Security</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/gas.svg"
+                            />Piped Gas</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/lift.svg"
+                            />Lift</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/security.svg"
+                            />24X7 Security</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/running.svg"
+                            />Jogging Track</li>
+                        <li className={styles.iconBox}>
+                            <img
+                                className={styles.icon}
+                                src="/images/staff.svg"
+                            />Maintenance Staff</li>
                     </ul>
                 </div>
             </div>
