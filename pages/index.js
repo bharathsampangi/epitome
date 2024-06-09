794d187e623860216680122229574b1b6800907b
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-09 12:03:50 +0530

    clicky

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,17 @@ export default function Home() {
         <title>Sumadhura Epitome Rachenahalli, Price/Reviews by Sumadhura Group</title>
         <meta charset="utf-8"/>
         <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-        <meta name="description" content="Sumadhura Epitome is a Brand New Residential Project at Bangalore by Sumadhura Group. It Offers 2 & 3 BHK Apartments. For More Details Contact Us Now." />
-        <meta name="keywords" content="Sumadhura Epitome,Sumadhura Epitome Rachenahalli,Sumadhura epitome price, Sumadhura epitome location,Sumadhura Epitome Reviews,Sumadhura Group,Sumadhura Epitome Thanisandra,Sumadhura Epitome North Bangalore, sumadhura epitome Brochures "/>
+        <meta name="description" content="Sumadhura Epitome is a Brand New Residential Project at North Bangalore by Sumadhura Group. It Offers 2 & 3 BHK Apartments. For More Details Contact Us Now." />
+        <meta name="keywords" content="Sumadhura Epitome,Sumadhura Epitome North Bangalore,Sumadhura epitome price, Sumadhura epitome location,Sumadhura Epitome Reviews,Sumadhura Group,Sumadhura Epitome Thanisandra,Sumadhura Epitome Rachenahalli, sumadhura epitome Brochures "/>
         <link rel="icon" href="/favicon.ico" />
         <meta name="robots" content="index, follow" />
         <link rel="canonical" href="https://sumadhuraepitome.in.net/"></link>
         <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
         <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
         <link rel="shortlink" href="https://sumadhuraepitome.in.net/"></link>
+        <a title="Privacy-friendly Web Analytics" href="https://clicky.com/101455692"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+        <script async data-id="101455692" src="//static.getclicky.com/js"></script>
+        <script async data-id="101455692" src="/8a1f613fd9d4.js"></script>
       </Head>
 
       <Main />
