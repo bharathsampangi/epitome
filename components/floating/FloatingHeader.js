d2bc9ac02ce182cdf237e019b7c9eb5c5c690c6b
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-09 21:13:03 +0530

    button

--- components/floating/FloatingHeader.js
+++ components/floating/FloatingHeader.js
@@ -2,13 +2,23 @@ import styles from "./styles/FloatingHeader.module.css"
 
 export default function FloatingHeader() {
     return (
-        <div className={styles.floating}>
-            <button className={styles.button}>
-                <a href="tel:7625039382" className={styles.link}><img className={styles.icon} src="/images/phone.svg" alt="Let's talk" /> Call Us</a>
-            </button>
-            <button className={styles.buttonWhatsapp}>
-                <a href="https://wa.link/0m3f8q" className={styles.link}><img className={styles.icon} src="/images/whatsapp.svg" alt="Let's Chat" /> Chat With Us</a>
-            </button>
-        </div>
+        <>
+            <div className={styles.floating}>
+                <button className={styles.button}>
+                    <a href="tel:7625039382" className={styles.link}><img className={styles.icon} src="/images/phone.svg" alt="Let's talk" /> Call Us</a>
+                </button>
+                <button className={styles.buttonWhatsapp}>
+                    <a href="https://wa.link/0m3f8q" className={styles.link}><img className={styles.icon} src="/images/whatsapp.svg" alt="Let's Chat" /> Chat With Us</a>
+                </button>
+            </div>
+            <div className={styles.floatingDesktop}>
+                <button className={styles.button}>
+                    <a href="tel:7625039382" className={styles.link}><img className={styles.icon} src="/images/phone.svg" alt="Let's talk" /> Call Us</a>
+                </button>
+                <button className={styles.buttonWhatsapp}>
+                    <a href="https://wa.link/0m3f8q" className={styles.link}><img className={styles.icon} src="/images/whatsapp.svg" alt="Let's Chat" /> Chat With Us</a>
+                </button>
+            </div>
+        </>
     )
 }
