186ea9ecb7debade2f28507f9d646b3002a0d427
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-10 18:58:04 +0530

    image change

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -6,16 +6,9 @@ export default function Hero({openModal}) {
         <section>
             <div className={styles.landingMain}>
                 <div className={styles.banner}>
-                    <p className={styles.small}>Pre-Launching</p>
-                    <h1 className={styles.header}>Sumadhura Epitome</h1>
-                    <p>Premium 2, 3 & 4 BHK apartments</p>
-                    <p>Rachenahalli, Thanisandra, North Bangalore</p>
                     <button className={styles.listButton} onClick={openModal} >
                         Download Brochure
                     </button>
-                    <div className={styles.call}>
-                        <a href="tel:7625039382" className={styles.bold}>Schedule Visit: +91 76250 39382</a>
-                    </div>
                 </div>
             </div>
         </section>
