9272592b2e5ad77676ec8df466529aab9b04c6c6
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-10 19:56:04 +0530

    hero

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -5,6 +5,9 @@ export default function Hero({openModal}) {
     return (
         <section>
             <div className={styles.landingMain}>
+                <div className={styles.banner}>
+                    <h1 className={styles.header}>Sumadhura Epitome</h1>
+                </div>
             </div>
         </section>
     );
