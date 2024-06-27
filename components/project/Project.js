3592f8c5a50d73b7fc0d2f35e08a5b76dd0f9af2
bharathsampangi <bharathsampangi95@gmail.com>
2024-06-27 07:22:34 +0530

    update

--- components/project/Project.js
+++ components/project/Project.js
@@ -17,43 +17,49 @@ export default function Project({openModal}) {
                         </li>
                         <li>
                             <span className={styles.title}>Area: </span>
-                            <button className={styles.button} onClick={openModal} >
-                                Get Details
-                            </button>
+                            <span>1.8 Acres</span>
                         </li>
                         <li>
                             <span className={styles.title}>
                                 No. of Towers:{" "}
                             </span>
-                            <button className={styles.button} onClick={openModal} >
-                                Get Details
-                            </button>
+                            <span>
+                                3 Towers
+                            </span>
                         </li>
                         <li>
-                            <span className={styles.title}>Structure: </span>
-                            <button className={styles.button} onClick={openModal} >
-                                Get Details
-                            </button>
+                            <span className={styles.title}>Floors: </span>
+                            <span>
+                                2B+G+14
+                            </span>
                         </li>
                         <li>
                             <span className={styles.title}>No. of Units: </span>
-                            <button className={styles.button} onClick={openModal} >
-                                Get Details
-                            </button>
+                            <span>
+                                262 Units
+                            </span>
                         </li>
                         <li>
                             <span className={styles.title}>
                                 Unit Variants:{" "}
                             </span>
-                            <span>2, 3 & 4 BHK</span>
+                            <span>2, 3 & 3.5 BHK</span>
                         </li>
                         <li>
                             <span className={styles.title}>Size Range: </span>
-                            <span>1280 - 2100 Sq.Ft</span>
+                            <span>1270 - 2075 Sq.Ft</span>
+                        </li>
+                        <li>
+                            <span className={styles.title}>Theme: </span>
+                            <span>Mediterranean Inspired Theme Project </span>
                         </li>
                         <li>
                             <span className={styles.title}>Price Range: </span>
                             <span>Starts from ₹1.27Cr* Onwards</span>
+                        </li>
+                        <li>
+                            <span className={styles.title}>Amenities: </span>
+                            <span>40+ Amenities with 18,000 Sqft Clubhouse </span>
                         </li>
                         <li>
                             <span className={styles.title}>
