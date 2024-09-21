8d8da796b8033253211f5290a10c99fe2a072ce0
bharathsampangi <bharathsampangi95@gmail.com>
2024-09-21 14:10:06 +0530

    preview

--- components/Pricing/Pricing.js
+++ components/Pricing/Pricing.js
@@ -8,9 +8,9 @@ export default function Pricing({openModal}) {
                 <h3 className={styles.header}>Pricing Structure</h3>
                 <div className={styles.priceContainer}>
                     <PriceBox unitName='2 BHK' unit='2 BHK' area='1270 Sq.Ft.' price='1.27 Cr* Onwards' openModal={openModal}/>
-                    <PriceBox unitName='3 BHK (Smart)' unit='3 BHK' area='1545 - 1610 Sq.Ft.' price='1.54 Cr* Onwards' openModal={openModal}/>
-                    <PriceBox unitName='3 BHK (Classic)' unit='3 BHK' area='1670 - 1830 Sq.Ft.' price='1.67 Cr* Onwards' openModal={openModal}/>
-                    <PriceBox unitName='4 BHK' unit='4 BHK' area='2075 Sq.Ft.' price='2.07 Cr* Onwards' openModal={openModal}/>
+                    <PriceBox unitName='2 BHK + STUDY' unit='2.5 BHK' area='1545 - 1610 Sq.Ft.' price='1.54 Cr* Onwards' openModal={openModal}/>
+                    <PriceBox unitName='Classic 3 BHK ' unit='3 BHK' area='1670 - 1830 Sq.Ft.' price='1.67 Cr* Onwards' openModal={openModal}/>
+                    <PriceBox unitName='3.5 BHK + STUDY' unit='3.5 BHK' area='2075 Sq.Ft.' price='2.07 Cr* Onwards' openModal={openModal}/>
                 </div>
             </div>
         </section>
